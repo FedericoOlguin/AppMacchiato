23f825271c02c5e6bf4681ae6b12484f35aaa9f3
Hernan Funes <funes.hernan@hotmail.es>
2022-04-05 13:13:48 -0300

    agregado redux de productos

---
+++ frontend/src/redux/reducers/productReducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+    allProducts: [],
+    oneProduct:[],
+};
+
+const productReducer = (state= initialState, action) => {
+    switch (action.type) {
+        case 'get_products':
+            return{
+                ...state,
+                allProducts: action.payload,
+            };
+        case 'get_one_product':
+            return{
+                ...state,
+                oneProduct:action.payload
+            };
+        default:
+            return state;
+
+    }
+
+}
