a694b644a3dfed2e714e0d68393e7222c6417168
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-04-05 02:33:10 -0300

    Passport/Validador/snackbar

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,37 @@
 const Router = require('express').Router()
 //const passport 
-//const validator
+const validator=require("../config/validacion")
+
+
+//rutas de usuarios
+const { signInUser, signUpUser, signOutUser, verifyEmail, verificarToken } = require("../controllers/ususariosController")
+
+// rutas sigIn/signUp user
+Router.route("/auth/signUp")
+    .post(validator, signUpUser)
+
+Router.route("/auth/signIn")
+    .post(signInUser)
+
+Router.route("/auth/signOut")
+    .post(signOutUser)
+
+// rutas para verificar token y email
+Router.route("/verify/:uniqueString")
+    .get(verifyEmail)
+
+Router.route("/auth/signInToken")
+    .get(passport.authenticate("jwt", { session: false }), verificarToken)
 
 
 
 //Rutas de productos
-const {getAllProducts, loadProduct, getOneProduct} = require('../controllers/productControllers')
+const { getAllProducts, loadProduct, getOneProduct } = require('../controllers/productControllers')
+
 
 Router.route('/allproducts')
-.get(getAllProducts)
-.post(loadProduct)
+    .get(getAllProducts)
+    .post(loadProduct)
 
 Router.route('/allproducts/:id')
-.get(getOneProduct)
+    .get(getOneProduct)
