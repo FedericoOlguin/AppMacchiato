95897bd2a137e2c8306fab135a5da7958aeafdd2
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-04-05 09:36:47 -0300

    rutas action

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -5,7 +5,7 @@ const usersActions = {
     signUp: (objUser) => {
         return async (dispatch, getState) => {
             try {
-                const res = await axios.post(`https://mytinerarry-olguin.herokuapp.com/api/auth/signUp`, { objUser })
+                const res = await axios.post(`http://localhost:3000/api/auth/signUp`, { objUser })
 
                 dispatch({
                     type: "message",
@@ -24,7 +24,7 @@ const usersActions = {
     signIn: (objUser) => {
         return async (dispatch, getState) => {
             try {
-                const res = await axios.post(`https://mytinerarry-olguin.herokuapp.com/api/auth/signIn`, { objUser })
+                const res = await axios.post(`http://localhost:3000/api/auth/signIn`, { objUser })
                 if (res.data.success) {
                     localStorage.setItem("token", res.data.response.token)
                     dispatch({ type: "user", payLoad: res.data })
@@ -47,7 +47,7 @@ const usersActions = {
         // console.log(userEmail)
         return async (dispatch, getState) => {
             try {
-                const res = await axios.post(`https://mytinerarry-olguin.herokuapp.com/api/auth/signOut`, { userEmail })
+                const res = await axios.post(`http://localhost:3000/api/auth/signOut`, { userEmail })
                 localStorage.removeItem("token")
                 console.log(res)
                 dispatch({ type: "userSignOut", payLoad: null })
@@ -63,7 +63,7 @@ const usersActions = {
         return async (dispatch, getState) => {
             try {
 
-                const res = await axios.get(`https://mytinerarry-olguin.herokuapp.com/api/auth/signInToken`, {
+                const res = await axios.get(`http://localhost:3000/api/auth/signInToken`, {
                     headers: {
                         Authorization: "Bearer " + token   //dejar espacio en bearer antes del cierre de las comillas ( "Bearer ")
                     }
