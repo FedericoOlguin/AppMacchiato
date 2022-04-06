cdd29aea494ef7dacdd5de8fd34e22b4d5389eb8
Sebazt <juan.londono29@udea.edu.co>
2022-04-06 10:28:39 -0500

    footer ok

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../Styles/FooterM.css"
 
 import InstagramIcon from '@mui/icons-material/Instagram';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import RoomIcon from '@mui/icons-material/Room';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import { Link as LinkRouter } from "react-router-dom"
 import logo from '../img/logo.png'
@@ -27,7 +27,8 @@ const Footer = () => {
       <div className="divIcon">
         <ul>
           
-          <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="mailto:macchiatoweb10@hmail.com"><MailOutlineIcon className="iconFooter" /></a></li>
+          <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="https://www.google.es/maps/place/ONI+Coffee+Shop/@48.8688602,2.3555228,17z/data=!3m1!4b1!4m5!3m4!1s0x47e66e0e33f60c7b:0x509e16fe7178f322!8m2!3d48.8688567!4d2.3577115?hl=es"><RoomIcon className="iconFooter"/></a></li>
+
           <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="https://www.instagram.com/"><InstagramIcon className="iconFooter" /></a></li>
           <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="https://www.whatsapp.com/"><WhatsAppIcon className="iconFooter"/></a></li>
         </ul>
