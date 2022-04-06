82e5b384253e13746a84539bf7ab1c5576774ec5
Daniel Martinez <danielfm.c222@hotmail.com>
2022-04-06 01:07:15 -0500

    commit x

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import Home from './Pages/Home';
 import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
+import ProductsIndex from './components/ProductsIndex';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
+
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
-
+        
       </Routes>
+        <ProductsIndex />
+      <Footer />
     </BrowserRouter>
   );
 }
