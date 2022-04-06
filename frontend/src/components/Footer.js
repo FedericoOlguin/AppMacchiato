ea7a68d265c1733bb94fe4133de95ed42e2d5222
Sebazt <juan.londono29@udea.edu.co>
2022-04-06 09:14:54 -0500

    before footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,58 +1,47 @@
-import React from 'react'
+import React from "react";
 import "../Styles/FooterM.css"
-/* import ImagenHeder from "../img/imagenHeader.png" */
-/* import FondoFooter from "../img/fondoHeader.jpg" */
-import { Link as LinkRouter } from 'react-router-dom';
+import facebook from "../img/IconFacebook.png"
+import instagram from "../img/IconInstagram.png"
+import twitter from "../img/IconTwitter.png"
+import whatsapp from "../img/IconWhatsapp.png"
+import { Link as LinkRouter } from "react-router-dom"
 
 
-import logo from "../img/logo.png"
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBitcoin, faFacebook, faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons"
 
 
 const Footer = () => {
   return (
+    <footer>
+      <div className="divNavFooter">
+        <ul>
+          <li className=""><LinkRouter className="footerNav" aria-current="page" to="/">Home</LinkRouter></li>
+          <li className=""><LinkRouter className="footerNav " to="/cities">Cities</LinkRouter></li>
+          <li><LinkRouter className="footerNav" to="#">Log In</LinkRouter></li>
+          <li><LinkRouter className="footerNav" to="#">Sign Up</LinkRouter></li>
 
-    <div className='globalFooter'>
+        </ul>
+      </div>
+      <div className="diVFooter">
 
-      {/* <img src={""} className="imagenFooter"/> */}
-
-      <div className='footerContainer'>
-
-        <div className='primerElemento'>
-          <img alt='logo' src={logo} />
-          <h3>Our Stores:</h3>
-          <p>Buenos Aires, Arg.</p>
-          <p>Medellín, Col.</p>
-          <p>Lima, Per.</p>
-        </div>
-
-        <div className='socialMedia'>
-          <h3>Follow us on social media</h3>
-          <div className='iconsMedia'>
-            <FontAwesomeIcon className='iconoMedia' icon={faFacebook} />
-            <FontAwesomeIcon className='iconoMedia' icon={faInstagram} />
-            <FontAwesomeIcon className='iconoMedia' icon={faWhatsapp} />
-            <FontAwesomeIcon className='iconoMedia' icon={faBitcoin} />
-          </div>
-        </div>
-
-        <div className='explore'>
-          <h3>Explore:</h3>
-          {/* <LinkRouter className='enlace' to="/inicio" >HOME</LinkRouter> */}
-          <LinkRouter className='enlace' to="/cities" >COFFE</LinkRouter>
-          <LinkRouter className='enlace' to="/cities" >STORE</LinkRouter>
-        </div>
+        <p><LinkRouter className="navbar-brand linkLogo" to="/">MyTinerary</LinkRouter></p>
+        <p>Contact: <a className="mail" href="http://gmail.com/" target="_blank" rel="nopener noreferrer">xxxxx@gmail.com</a></p>
+        <p>Adress: San Martín 4598  Capital Federal.</p>
 
 
       </div>
-      <h2>MACCHIATTO STORES © 2022</h2>
-    </div>
+      <div className="divIcon">
+        <ul>
+          <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="https://facebook.com/"><img className="iconFooter" src={facebook} alt="iconoFacebook" /></a></li>
+          <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="https://twitter.com/"><img className="iconFooter" src={twitter} alt="iconoTwitter" /></a></li>
+          <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="https://www.instagram.com/"><img className="iconFooter" src={instagram} alt="iconoInstagram" /></a></li>
+          <li><a className="nav-link" target="_blank" rel="noreferrer nopener" href="https://www.whatsapp.com/"><img className="iconFooter" src={whatsapp} alt="iconoWhatsapp" /></a></li>
+        </ul>
 
-
-
+      </div>
+      <div className="divCopy">
+        <h6 >Copyrigth&copy; 2022 All rights reserved</h6>
+      </div>
+    </footer>
   )
 }
-
 export default Footer
