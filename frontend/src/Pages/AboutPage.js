5752b48ea8c4b11b585a6c143563e0f1e8beadc9
Sebazt <juan.londono29@udea.edu.co>
2022-04-07 11:35:17 -0500

    about us page finish

--- frontend/src/Pages/AboutPage.js
+++ frontend/src/Pages/AboutPage.js
@@ -2,15 +2,21 @@ import React from 'react'
 import '../Styles/aboutPage.css'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
-
+import VideoMacchiato from '../img/videoMacchiato.mp4';
+import fernanda from '../img/fernanda.jpg';
+import marcia from '../img/marcia.jpg'
+import fede from '../img/federico.jpg'
+import sebas from '../img/sebastian.jpg'
+import dani from '../img/daniel.jpg'
+import hernan from '../img/hernan.jpg'
 
 function AboutPage() {
   return (
     <div className='fondo-AboutUs'>
       <NavBar />
       <div className='container-AboutPage'>
-        <div className='img-aboutUsPage'></div>          
-       
+        <div className='img-aboutUsPage'></div>
+
         <div className='container-ppal'>
           <div className='container-textoAbout'>
             <h1 className='tituloh1'>About Macchiato</h1>
@@ -23,27 +29,14 @@ function AboutPage() {
           </div>
 
           <div className='container-videoAbout'>
-            <video 
-            source={''}
-            />
-          </div>
-
-          <div className='container-textoAbout'>
-            <h2 className='tituloh2'>Get to know the Macchiato experience</h2>
-            <p className='parrafo-About'>Being able to enjoy Macchiato products in a relaxed atmosphere, and at
-              the same time sharing rewarding moments, were the reasons that gave
-              rise to the creation of the Macchiato Coffee concept. Macchiato is a
-              pioneer in the spread of specialized coffee Shop, places designed with
-              all the comfort and a relaxed atmosphere so that the customer can
-              enjoy the Premium quality of its products. Today the Macchiato Coffee
-              are part of the urban geography of the main cities throughout France
-              and other countries in the world. Macchiato is much more than the best
-              coffee, it is a consumption experience in a warm, modern and
-              innovative environment, the Macchiato Coffee. The ideal place to share
-              the richest coffee with a wide range of naturally made products.
-              Macchiato went from being the representation of Paris in the local
-              market to being an icon of France in the international market. And one
-              of the most representative ambassadors of France in the world.</p>
+            <video
+              autoPlay
+              loop
+              muted
+              className='video-AboutUs'
+            >
+              <source src={VideoMacchiato} type='video/mp4' />
+            </video>
           </div>
 
           <div className='container-textoAbout'>
@@ -59,33 +52,74 @@ function AboutPage() {
               the Macchiato Experience through our service style: warmth, service
               and passion.</p>
           </div>
+
+          <div className='container-textoAbout'>
+            <h2 className='tituloh2'>Get to know the Macchiato experience</h2>
+            <p className='parrafo-About'>Being able to enjoy Macchiato products in a relaxed atmosphere, and at
+              the same time sharing rewarding moments, were the reasons that gave
+              rise to the creation of the Macchiato Coffee concept. Macchiato is a
+              pioneer in the spread of specialized coffee Shop, places designed with
+              all the comfort and a relaxed atmosphere so that the customer can
+              enjoy the Premium quality of its products.</p>
+          </div>
+
+
         </div>
       </div>
 
+      <h1 className='h1-developers'>Our passionate team of developers</h1>
       <div className='contenedor-developers'>
-        <h1 className='h1-developers'>Our passionate team of developers</h1>
         <div className='fotos-developers'>
+
           <div className='fotos-1ercontenedor'>
+
             <div className='contenedor-uno'>
-
+              <div className='contenedor-devimg'>
+                <a target="_blank" rel="noreferrer nopener" href="https://www.linkedin.com/in/marcia-montivero-gutierrez-383009207/"><img  src={marcia} alt='desarrollador' className='img-dev' /></a>
+              </div>
+              <h3 className='nombreDesarrollador'>Marcia Montivero</h3>
             </div>
+
             <div className='contenedor-dos'>
-
+              <div className='contenedor-devimg'>
+                <a target="_blank" rel="noreferrer nopener" href="https://www.linkedin.com/in/federico-olgu%C3%ADn-1b405b201/"><img src={fede} alt='desarrollador'className='img-dev' /></a>
+              </div>
+              <h3 className='nombreDesarrollador'>Federico Olguín</h3>
             </div>
+
             <div className='contenedor-tres'>
-
+              <div className='contenedor-devimg'>
+                <a target="_blank" rel="noreferrer nopener" href="https://www.linkedin.com/in/hernan-funes-50301421b/"><img src={hernan} alt='desarrollador'className='img-dev' /></a>
+              </div>
+              <h3 className='nombreDesarrollador'>Hernán Funes</h3>
             </div>
+
           </div>
+
           <div className='fotos-2ercontenedor'>
+
             <div className='contenedor-uno'>
-
+              <div className='contenedor-devimg'>
+                <a target="_blank" rel="noreferrer nopener" href="https://www.linkedin.com/in/fernanda-arrieta-755665b0/"><img src={fernanda} className='img-dev' alt='desarrollador'  /></a>
+              </div>
+              <h3 className='nombreDesarrollador'>Fernanda Arrieta</h3>
             </div>
+
             <div className='contenedor-dos'>
+              <div className='contenedor-devimg'>
+                <a target="_blank" rel="noreferrer nopener" href="https://www.linkedin.com/in/juan-sebastian-londo%C3%B1o-cossio-9444781b7/"><img src={sebas} alt='desarrollador' className='img-dev' /></a>
+              </div>
 
+              <h3 className='nombreDesarrollador'>Sebastian Londoño</h3>
             </div>
+
             <div className='contenedor-tres'>
-
+              <div className='contenedor-devimg'>
+                <a target="_blank" rel="noreferrer nopener" href="https://www.linkedin.com/in/daniel-mart%C3%ADnez-casta%C3%B1eda-8b363b15a/"><img src={dani} alt='desarrollador' className='img-dev' /></a>
+              </div>
+              <h3 className='nombreDesarrollador'>Daniel Martinez</h3>
             </div>
+
           </div>
         </div>
       </div>
