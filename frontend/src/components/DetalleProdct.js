df3f8ea118c8c413407af2b85c7a858d9133f29e
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-04-09 04:00:01 -0300

    Detalle producto sin responsive

--- frontend/src/components/DetalleProdct.js
+++ frontend/src/components/DetalleProdct.js
@@ -34,27 +34,32 @@ const Detalle = (props) => {
             <NavBar />
             {console.log(prod)}
             <div className="hero">
-                
+
                 <div className="product">
-                    <div className="primercontenedor-detalle">
-                        <h1 className="titulo-detalle">{prod.name}</h1>
+                    <div className="contenedorBotonImg">
+                        <div className="buy-detalle">
+                            <button className='button-detalle'><span class="text">Add Store</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
+                        </div>
                         <img
                             alt="DetalleProducto"
                             src={prod.image}
                             className="imagen-detalle"
                         />
                     </div>
-                    <div className="p-detalle">
-                        <p>{prod.description}</p>
-                    </div>
-                    <div className="priceStock-detalle">
-                        <p>$ {prod.price}</p>
-                        <p>Stock: {prod.stock}</p>
+                    <div className="textCard">
+                        <div className="primercontenedor-detalle">
+                            <h1 className="titulo-detalle">{prod.name}</h1>
+                        </div>
+                        <div className="p-detalle">
+                            <p>{prod.description}</p>
+                        </div>
+                        <div className="priceStock-detalle">
+                            <p>$ {prod.price}</p>
+                            <p>Stock: {prod.stock}</p>
+                        </div>
                     </div>
 
-                    <div className="buy-detalle">
-                        <button className='button-detalle'><span class="text">Add Store</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
-                    </div>
+
                 </div>
 
             </div >
