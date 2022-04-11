5585f6a3dac3cff6bf1b1888e7cda797ed047ed9
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-04-11 02:02:03 -0300

    build

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const passport = require("passport")
 const cors = require("cors")
 const express = require("express")
 require("./config/database")
-
-
 const Router = require("./routes/routes")
-const PORT = 4000
 const app = express()
+
+
+
+
+
+const PORT = process.env.PORT || 4000
+const HOST = process.env.HOST || '0.0.0.0'
+const path = require("path")
 
 
 app.use(cors())
@@ -15,4 +20,13 @@ app.use(express.json())
 app.use("/api", Router)
 app.use(passport.initialize())
 
-app.listen(PORT, () => console.log("Server ready on PORT " + PORT))
+if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + "/client/build/index.html"))
+    })
+
+}
+
+app.listen(PORT, HOST, () => console.log(`Server ready on PORT ` + PORT))
