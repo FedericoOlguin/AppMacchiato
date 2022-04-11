302c237924f2ab58218adb72b23e8e3def96fc66
Hernan Funes <funes.hernan@hotmail.es>
2022-04-11 15:44:55 -0300

    front modify products

---
+++ frontend/src/components/CardModify.js
@@ -0,0 +1,80 @@
+// import { connect } from "react-redux";
+import React, { useState, useEffect, useRef } from "react";
+import FormModify from "./FormModify";
+
+
+
+function CardModify(props) {
+
+    const [form3, setForm3] = useState(false)
+    function openForm3() {
+        setForm3(!form3)
+      }
+
+  return (
+    <>
+    {props.form2?(
+        <>
+        <div className="card-panel">
+            <div className="panel-imgproducto">
+            <img
+                src={props.product.image}
+                alt="producto"
+                className="imagen-producto"
+            />
+            </div>
+            <div className="items-producto">
+                <h2 className="subtitulo-producto">Nombre producto</h2>
+                <h2 className="respuesta-producto"> {props.product.name}</h2>
+                <h2 className="subtitulo-producto">Precio</h2>
+                <h2 className="respuesta-producto">{props.product.price}</h2>
+            </div>
+            <div className="items-producto">
+                <h2 className="subtitulo-producto">Stock</h2>
+                <h2 className="respuesta-producto"> {props.product.stock}</h2>
+                <h2 className="subtitulo-producto">Sale</h2>
+                <h2 className="respuesta-producto"> {props.product.sale?'On Sale':'not on Sale'}</h2>
+            </div>
+        </div>
+
+        <div className="boton-panel">
+            <button className="button-modify" onClick={openForm3} >
+                <span class="text">Modificar</span>
+                <span class="icon">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    >
+                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                    </svg>
+                </span>
+            </button>
+
+            <button className="button-elimintate">
+                <span class="text">Delete</span>
+                <span class="icon">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    >
+                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                    </svg>
+                </span>
+            </button>
+        </div>
+        <FormModify form3={form3} product={props.product}/>
+        </>
+        
+        ):(
+            <>
+            </>
+        )}
+    </>
+  );
+}
+
+export default CardModify
---
+++ frontend/src/components/FormModify.js
@@ -0,0 +1,146 @@
+import React, { useRef } from 'react'
+import '../Styles/panelproducts.css'
+import { connect } from "react-redux"
+import productActions from "../redux/actions/productActions"
+
+function FormPanel(props) {
+    let formProd = useRef()
+
+
+    // function enviarData(event) {
+    //     event.preventDefault()
+    //     formProd.current.focus()
+    //     let boolean
+    //     let datosInp = new FormData(formProd.current)
+    //     console.log(datosInp.get("sale"));
+    //     if (datosInp.get("sale") === "on") {
+    //         boolean = true
+    //     }else{
+    //         boolean=false
+    //     }
+    //     let productObj = {
+    //         name: datosInp.get("nameItem"),
+    //         image: datosInp.get("ItemPhotoUrl"),
+    //         category: datosInp.get("category"),
+    //         price: datosInp.get("price"),
+    //         stock: datosInp.get("stock"),
+    //         description: datosInp.get("description"),
+    //         flag: datosInp.get("flag"),
+    //         sale: boolean
+    //     }
+    //     props.loadProduct(productObj, props.user.id)
+
+    // }
+
+    return (
+        <>
+
+            {props.form3 ? (
+                <div id='form-signup-panelProducts'>
+                    <main className="mainForm-panelProducts">
+                        <h2 className='subtitle-signup-panelProducts'>Modify product </h2>
+                        <div className="formContainer-panelProducts">
+                            <form action="" className="form-panelProducts" 
+                            // onSubmit={enviarData} 
+                            ref={formProd} id="form">
+
+                                <fieldset className="fieldsetFrom-panelProducts">
+
+                                    <label className="labelForm-seetings" htmlFor="nameItem">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Name item</span>
+                                        </div>
+                                        <input className="inputFrom-panelProducts" value={props.product.name} type="text" id="nameItem" name="nameItem" required  />
+                                    </label>
+
+                                    <label className="labelForm-panelProducts" htmlFor="ItemPhotoUrl">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Item photoURL</span>
+                                        </div>
+                                        <input className="inputFrom-panelProducts" value={props.product.image} type="text" id="ItemPhotoUrl" name="ItemPhotoUrl" />
+                                    </label>
+
+                                    <label className="labelForm-panelProducts" htmlFor="category">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Category</span>
+                                        </div>
+                                        <input className="inputFrom-panelProducts" value={props.product.category} type="text" id="category" name="category" />
+                                    </label>
+
+
+                                    <label className="labelForm-panelProducts" htmlFor="stock">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Stock</span>
+                                        </div>
+                                        <input className="inputFrom-panelProducts" value={props.product.stock} type="text" id="stock" name="stock" />
+                                    </label>
+                                    <label className="labelForm-panelProducts" htmlFor="price">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Price</span>
+                                        </div>
+                                        <input className="inputFrom-panelProducts" value={props.product.price} type="text" id="price" name="price" />
+                                    </label>
+
+
+                                    <label className="labelForm-panelProducts" htmlFor="description">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Description</span>
+                                        </div>
+                                        <input className="inputFrom-panelProducts" value={props.product.description} type="text" id="description" name="description" />
+                                    </label>
+
+                                    <label className="labelForm-panelProducts" htmlFor="sale">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Sale</span>
+                                            <input className="" type="checkbox" id="sale" name="sale" checked={props.product.sale} />
+                                        </div>
+                                    </label>
+
+                                    <label className="labelForm-panelProducts" htmlFor="flag">
+                                        <div className='container-panelProduct'>
+                                            <span className='span-signup-panelProducts'>Flag photoURL</span>
+                                        </div>
+                                        <input className="inputFrom-panelProducts" value={props.product.flag} type="text" id="flag" name="flag" />
+                                    </label>
+
+                                    <button className="button-panelProducts" type="submit">
+                                        <div className="svg-wrapper-1">
+                                            <div className="svg-wrapper">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                                    <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <span>Send</span>
+                                    </button>
+                                </fieldset>
+                            </form>
+                        </div>
+
+                    </main>
+                </div>
+
+            ) : (
+                <></>
+
+            )}
+        </>
+    )
+}
+
+// export default FormPanel
+
+
+const mapStateToProps = (state) => {
+    return {
+        allProducts: state.productReducer.allProducts,
+        filtered: state.productReducer.filtered,
+        user: state.userReducer.user,
+    }
+}
+const mapDispatchToProps = {
+    loadProduct: productActions.loadProduct
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FormPanel)
